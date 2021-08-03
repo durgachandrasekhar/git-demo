b04b625a41b97ed1f4444760ab3a785d3b58411d
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-03 11:22:59 +0530

    feat:added message

--- git demo/Program.cs
+++ git demo/Program.cs
@@ -7,12 +7,8 @@ namespace git_demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-<<<<<<< HEAD
-            Console.WriteLine("Hi Bugatti");
-            Console.WriteLine("How are you");
-=======
             Console.WriteLine("Hi Ferrari");
->>>>>>> cb305e3de4aa3463258ed348318ef34760bc16c6
+            Console.WriteLine("How are you");
         }
     }
 }
